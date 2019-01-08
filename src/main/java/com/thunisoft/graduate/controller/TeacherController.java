12099f06920f84b40192d19f74fe465f194f1e3c
刘野 <liuye@thunisoft.com>
2019-01-08 16:56:45 +0800

    教师模块的CURD已经完成

--- src/main/java/com/thunisoft/graduate/controller/TeacherController.java
+++ src/main/java/com/thunisoft/graduate/controller/TeacherController.java
@@ -4,12 +4,7 @@ import com.thunisoft.graduate.common.Constants;
 import com.thunisoft.graduate.common.model.Teacher;
 import com.thunisoft.graduate.service.ITeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author: mirror6
@@ -23,26 +18,66 @@ public class TeacherController {
     /**
      * 注入teacher的服务层
      */
+    private final ITeacherService teacherService;
+
     @Autowired
-    private ITeacherService teacherService;
+    public TeacherController(ITeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
 
 
     /**
      * 添加教师信息
-     * 增加 restful 风格
-     * url:"http://localhost/student/addone"
+     * url:"http://localhost:8080/teacher/addTeacher"
      * 通过<form>表单模拟验证
-     *
-     * @param
      */
     // == @RequestMapping(value="/addTeacher",method=RequestMethod.POST)
-    @PostMapping("/addTeacher")
     @ResponseBody
-    public String addTeacher() throws Exception {
-
+    @PostMapping("/addTeacher")
+    public String addTeacher() {
 //        String id=request.getParameter("");
-        Teacher teacher = new Teacher(201213, "蓝忘机", 1, "计算机工程学院", "1918", "18341114205", "");
+        Teacher teacher = new Teacher(201213, "蓝曦臣", 1, "计算机工程学院", "1918", "18341114205", "");
         teacherService.addTeacher(teacher);
+        return Constants.C_SUCCESS;
+    }
+
+    /**
+     * 删除教师信息
+     * url:"http://localhost:8080/teacher/deleteTeacher/{id}"
+     * 注意无法通过浏览器的链接来模拟检验,可以通过 jquery的 $.ajax方法，并type="delete"
+     */
+    // == @RequestMapping(value = "/deleteTeacher/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    @DeleteMapping("/deleteTeacherById/{id}")
+    public String deleteTeacherById(@PathVariable("id") Integer id) {
+        teacherService.deleteTeacherById(id);
+        return Constants.C_SUCCESS;
+    }
+
+    /**
+     * 修改教师信息
+     * url:"http://localhost:8080/teacher/updateTeacher"
+     * 验证：可以通过 jquery的 $.ajax方法，并type="put",同时注意data形式——A=a&B=b&C=c
+     */
+    // == @RequestMapping(value="/updateTeacher",method=RequestMethod.PUT)
+    @ResponseBody
+    @PutMapping("/updateTeacher")
+    public String updateTeacher() {
+        Teacher teacher = new Teacher(201213, "123456", "蓝忘机", 1, "计算机工程学院", "1918", "18341114205", "");
+        teacherService.updateTeacher(teacher);
+        return Constants.C_SUCCESS;
+    }
+
+    /**
+     * 通过ID查找教师信息
+     * url:"http://localhost:8080/teacher/getTeacherById/{id}"
+     *
+     * @return Teacher
+     */
+    // == @RequestMapping(value = "/getTeacherById/{id}", method = RequestMethod.GET)
+    @GetMapping("/getTeacherById/{id}")
+    public String getTeacherById(@PathVariable("id") Integer id) {
+        teacherService.getTeacherById(id);
         return Constants.C_SUCCESS;
     }
 
